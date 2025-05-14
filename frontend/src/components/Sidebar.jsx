bd73a941472213180c0fb7d67771905521811f47
Sunder Singh <ssunder.singh052@example.com>
2025-05-14 13:49:14 +0530

    made some changes

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -27,13 +27,13 @@ const Sidebar = () => {
         <span>Home</span>
       </Link>
 
-      <Link to="/friends" className={`btn btn-ghost justify-start w-full gap-3 px-3 normal-case ${
+      {/* <Link to="/friends" className={`btn btn-ghost justify-start w-full gap-3 px-3 normal-case ${
         currentPath === "/friends" ? "btn-active" : ""
         }`}
       >
         <UserIcon className="start-5 text-base-content opacity-70" />
         <span>Friends</span>
-      </Link>
+      </Link> */}
 
       <Link to="/notification" className={`btn btn-ghost justify-start w-full gap-3 px-3 normal-case ${
         currentPath === "/notification" ? "btn-active" : ""
